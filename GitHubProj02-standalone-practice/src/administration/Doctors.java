4b4e9c82222fb1002b56a52a0e08543f529d69b8
LENOVO <LENOVO@192.168.1.38>
2025-08-12 11:00:50 +0530

    Dev1 adding nurses method into src>admitration>Doctors>nurses()

--- GitHubProj02-standalone-practice/src/administration/Doctors.java
+++ GitHubProj02-standalone-practice/src/administration/Doctors.java
@@ -5,5 +5,9 @@ public class Doctors {
 	public  Doctors(String DocName , String Speciality) {
 		System.out.println("this is doctor :"+DocName+" Expertise in :"+Speciality);
 	}
+	
+	public String nurses(String name , String type) {
+		return name+" is working nurse and her type is :";
+	}
 
 }
