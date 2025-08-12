70e6b145dd259131f53415381492becdfffcc1dc
LENOVO <LENOVO@192.168.1.38>
2025-08-12 10:53:21 +0530

    Dev2 worked in package administration and added doctors constructor

---
+++ GitHubProj02-standalone-practice/src/administration/Doctors.java
@@ -0,0 +1,9 @@
+package administration;
+
+public class Doctors {
+	
+	public  Doctors(String DocName , String Speciality) {
+		System.out.println("this is doctor :"+DocName+" Expertise in :"+Speciality);
+	}
+
+}
